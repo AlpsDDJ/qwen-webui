a3cd89540bc3590d4c21be37487ae13e9601663f
DDJ <AlpsDDJ@163.com>
2024-06-21 22:09:19 +0800

    更改页面标题为"千问 - WebUI"，修复Vue组件中的代码错误。

--- src/components/ChatBoxHeader/components/SelectChatModel.vue
+++ src/components/ChatBoxHeader/components/SelectChatModel.vue
@@ -1,18 +1,19 @@
 <script setup lang="ts">
-import {useChatSessionStore} from "@/store/ChatSession";
-import {useChatModelConfig} from "@/store/ChatModelConfig";
+import {useChatSessionStore} from '@/store/ChatSession'
+import {useChatModelConfig} from '@/store/ChatModelConfig'
 
 const {setCurrentModel, model} = useChatSessionStore();
 const {models} = storeToRefs(useChatModelConfig());
 const modelConfigOptions = computed(() => models.value.map(({name, id}) => ({label: name, value: id})))
-const selectModel = ref<string>(model?.id ?? '')
+const selectedModel = ref<string>(model?.id ?? '')
 const handleSelectModel = (value) => {
-  setCurrentModel(unref(models.value.find(item => item.id === value)))
+  const model = models.value.find(item => item.id === value);
+  model && setCurrentModel(unref(model))
 }
 </script>
 
 <template>
-  <n-select class="w-120px" v-model:value="selectModel" :options="modelConfigOptions" @update:value="handleSelectModel" placeholder="选择模型"/>
+  <n-select class="w-120px" v-model:value="selectedModel" :options="modelConfigOptions" @update:value="handleSelectModel" placeholder="选择模型"/>
 </template>
 
 <style scoped lang="less">
